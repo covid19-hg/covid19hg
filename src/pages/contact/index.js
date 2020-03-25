e58d1b421b1bb97a7e6341e17d684695c36d46b5
Huy Nguyen <me@huy.dev>
2020-03-25 17:56:53 -0400

    Rename recaptcha key env var

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,7 +6,7 @@ import Layout from "../../components/Layout";
 import { navigate } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY;
+const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY;
 if (typeof RECAPTCHA_KEY === "undefined") {
   throw new Error(`
   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined!
