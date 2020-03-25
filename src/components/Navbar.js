d5a7e3cbfb07d0e3cdb2bf9dca7e92bb563c7b20
Matthew Solomonson <ggmatt@gmail.com>
2020-03-25 10:25:28 -0400

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Kaldi" style={{ width: '150px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,8 +63,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/partners">
+                Partners
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -41,7 +41,7 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            backgroundColor: '#262262',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -54,7 +54,7 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            backgroundColor: '#262262',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
