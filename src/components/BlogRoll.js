f9914db222047718e36bf08ec69902ea15ffe8bc
Huy Nguyen <me@huy.dev>
2020-03-26 14:19:35 -0400

    Remove extraneous dot after blog post name

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -36,7 +36,7 @@ class BlogRoll extends React.Component {
                     >
                       {post.frontmatter.title}
                     </Link>
-                    <span> &bull; </span>
+                    <span></span>
                     <span className="subtitle is-size-5 is-block">
                       {post.frontmatter.date}
                     </span>
