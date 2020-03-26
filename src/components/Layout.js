5e28daca5376e2ce23709f8ed9837ab6ecbea989
Matthew Solomonson <ggmatt@gmail.com>
2020-03-26 11:04:36 -0400

    Update og-image in Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/header-4.png`}
         />
       </Helmet>
       <Navbar />
