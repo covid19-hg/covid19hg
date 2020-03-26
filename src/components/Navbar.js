1a5e176adf99d172b000145c79262d74e9b6eb14
Matthew Solomonson <ggmatt@gmail.com>
2020-03-25 22:15:49 -0400

    Blog --> News in header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = class extends React.Component {
                 Partners
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                News
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
