d1f03f002fefbbf96967f9458a862fb8653e2f7b
Matthew Solomonson <ggmatt@gmail.com>
2020-03-26 12:19:49 -0400

    Change position of card tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -26,8 +26,8 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
         <meta property="og:image" content={`${withPrefix('/')}img/header-4.png`} />
-        <meta property="twitter:image" content={`${withPrefix('/')}img/twitter-summary-large.png`} />
         <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content={`${withPrefix('/')}img/twitter-summary-large.png`} />
         <meta property="twitter:title" content="COVID-19 Host Genetics Initiative" />
         <meta
           property="twitter:description"
