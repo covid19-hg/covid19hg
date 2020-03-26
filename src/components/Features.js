15e3287b2552de213465a59c5a15684f795faeef
Matthew Solomonson <ggmatt@gmail.com>
2020-03-25 22:51:50 -0400

    Drop feature image width

--- src/components/Features.js
+++ src/components/Features.js
@@ -10,7 +10,7 @@ const FeatureGrid = ({ gridItems }) => (
           <div className="has-text-centered">
             <div
               style={{
-                width: '400px',
+                width: '300px',
                 display: 'inline-block',
               }}
             >
