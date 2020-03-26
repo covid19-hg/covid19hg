475917ed099195aecad8c02b7721394ebc80bf46
Matthew Solomonson <ggmatt@gmail.com>
2020-03-26 11:50:03 -0400

    Update twitter meta

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,41 +15,23 @@ const TemplateWrapper = ({ children }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${withPrefix('/')}img/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" href={`${withPrefix('/')}img/favicon-32x32.png`} sizes="32x32" />
+        <link rel="icon" type="image/png" href={`${withPrefix('/')}img/favicon-16x16.png`} sizes="16x16" />
 
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
+        <link rel="mask-icon" href={`${withPrefix('/')}img/safari-pinned-tab.svg`} color="#ff4400" />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
+        <meta property="og:image" content={`${withPrefix('/')}img/header-4.png`} />
+        <meta property="twitter:image" content={`${withPrefix('/')}img/header-4.png`} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="COVID-19 Host Genetics Initiative" />
         <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/header-4.png`}
-        />
-        <meta
-          property="twitter:image"
-          content={`${withPrefix('/')}img/twitter-preview.png`}
+          property="twitter:description"
+          content="The COVID-19 host genetics initiative aims to provide support and an analytical network for studies that are broadly interested in identifying genetic determinants of COVID-19 susceptibility and severity. Such discoveries could help to generate hypotheses for drug repurposing, identify individuals at unusually high or low risk, and contribute to global knowledge of the biology of SARS-CoV-2 infection and disease."
         />
       </Helmet>
       <Navbar />
