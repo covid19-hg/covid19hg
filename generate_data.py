8e75f53a3de9fa6697224a06e1d1f9692a0e05b2
Huy Nguyen <me@huy.dev>
2020-03-27 18:21:44 -0400

    Update Google Sheet data & create data for Mapbox studio

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,7 @@ import googlemaps
 from dotenv import load_dotenv
 import os
 import random
+import json
 
 load_dotenv(dotenv_path="./.env.local")
 client = googlemaps.Client(key=os.getenv("GOOGLE_API_KEY"))
@@ -35,23 +36,48 @@ df = pd.read_csv(
 )
 raw_coords = [get_coords(x) for x in df["city_country"]]
 
-adjusted_coords = []
+label_coords = []
+marker_coords = []
 for coord in raw_coords:
-    if coord in adjusted_coords:
+    if coord in label_coords:
         # Add some noise to duplicate coords so that they appear as separate markers on the map:
         (lng, lat) = coord
         new_lng = lng + random.uniform(-0.01, 0.01)
         new_lat = lat + random.uniform(-0.01, 0.01)
-        adjusted_coords.append((new_lng, new_lat))
+        label_coords.append((new_lng, new_lat))
     else:
-        adjusted_coords.append(coord)
+        label_coords.append(coord)
+    marker_coords.append(coord)
 
-lngs = [coord[0] for coord in adjusted_coords]
-lats = [coord[1] for coord in adjusted_coords]
+label_lngs = [coord[0] for coord in label_coords]
+label_lats = [coord[1] for coord in label_coords]
+marker_lngs = [coord[0] for coord in marker_coords]
+marker_lats = [coord[1] for coord in marker_coords]
 
-df = df.assign(lat=lats, lng=lngs)
-df = df[["lat", "lng", "study_biobank", "coordinator", "city_country"]]
+df = df.assign(lat=marker_lats, lng=marker_lngs,
+               label_lat=label_lats, label_lng=label_lngs)
+marker_df = df[["lat", "lng", "study_biobank", "coordinator", "city_country"]]
+label_df = df[["label_lat", "label_lng", "study_biobank"]]
 
-json_string = df.to_json(orient="records")
+json_string = marker_df.to_json(orient="records")
 with open("src/data.json", "w") as f:
     f.write(json_string)
+
+geojson_features = [
+    {
+        "type": "Feature",
+        "properties": {
+            "study_biobank": x[3],
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [x[2], x[1]]
+        }
+    }
+    for x in label_df.itertuples()]
+geojson = {
+    "type": "FeatureCollection",
+    "features": geojson_features
+}
+with open("mapbox-studio-data.geojson", "w") as f:
+    json.dump(geojson, f)
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,7 +14,7 @@ const initializeMap = el => {
 
   const mapboxMap = new MapboxMap({
     container: el,
-    style: "mapbox://styles/huy-nguyen/ck8alo7ay0uck1ipbpmdwggyz",
+    style: "mapbox://styles/huy-nguyen/ck8aqzc8n0x4d1int7403icph",
     accessToken: process.env.GATSBY_MAPBOX_API_KEY,
     renderWorldCopies: false,
     bounds: [southWest, northEast],
