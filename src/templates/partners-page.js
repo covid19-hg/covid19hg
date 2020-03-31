9de512bfc341fc4ce4e8edd15ec349be792f1310
Matthew Solomonson <ggmatt@gmail.com>
2020-03-31 09:48:53 -0400

    Replace table on partners page with Airtable view

--- src/templates/partners-page.js
+++ src/templates/partners-page.js
@@ -1,35 +1,32 @@
-import React, { useReducer } from "react";
-import PropTypes from "prop-types";
-import { graphql } from "gatsby";
-import Layout from "../components/Layout";
-import Map, {
-  SET_SELECTED_INSTITUTION_ACTION,
-  UNSET_SELECTED_INSTITUTION_ACTION
-} from "../components/Map";
-import InstitutionsList from "../components/InstitutionsList";
-import useCanonicalLinkMetaTag from "../components/useCanonicalLinkMetaTag";
+import React, { useReducer } from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Map, { SET_SELECTED_INSTITUTION_ACTION, UNSET_SELECTED_INSTITUTION_ACTION } from '../components/Map'
+import InstitutionsList from '../components/InstitutionsList'
+import useCanonicalLinkMetaTag from '../components/useCanonicalLinkMetaTag'
 
-const data = require("../data.json");
+const data = require('../data.json')
 
 const reducer = (state, action) => {
   switch (action.type) {
     case SET_SELECTED_INSTITUTION_ACTION:
-      return action.id;
+      return action.id
     case UNSET_SELECTED_INSTITUTION_ACTION:
-      return undefined;
+      return undefined
     default:
-      return state;
+      return state
   }
-};
+}
 
 export const ProductPageTemplate = ({ title }) => {
-  const [state, dispatch] = useReducer(reducer, undefined);
+  const [state, dispatch] = useReducer(reducer, undefined)
 
-  let details;
+  let details
   if (state === undefined) {
-    details = null;
+    details = null
   } else {
-    const info = data.find(({ id }) => id === state);
+    const info = data.find(({ id }) => id === state)
     details = (
       <>
         <h2 className="title is-4">Institute</h2>
@@ -39,7 +36,7 @@ export const ProductPageTemplate = ({ title }) => {
         <h2 className="title is-4">Coordinators</h2>
         <p>{info.coordinator}</p>
       </>
-    );
+    )
   }
 
   return (
@@ -48,10 +45,10 @@ export const ProductPageTemplate = ({ title }) => {
         <h1
           className="has-text-weight-bold is-size-1"
           style={{
-            boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-            backgroundColor: "#142166",
-            color: "white",
-            padding: "1rem"
+            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+            backgroundColor: '#142166',
+            color: 'white',
+            padding: '1rem',
           }}
         >
           {title}
@@ -71,40 +68,42 @@ export const ProductPageTemplate = ({ title }) => {
             <InstitutionsList />
           </div>
         </div>
+        <center>
+          <p>
+            <strong>Website team: </strong> Matthew Solomonson, Huy Nguyen
+          </p>
+        </center>
       </section>
     </div>
-  );
-};
+  )
+}
 
 ProductPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string
-};
+  title: PropTypes.string,
+}
 
 const ProductPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
-  const canonicalLinkMetaTag = useCanonicalLinkMetaTag("/partners/");
+  const { frontmatter } = data.markdownRemark
+  const canonicalLinkMetaTag = useCanonicalLinkMetaTag('/partners/')
 
   return (
     <Layout>
       {canonicalLinkMetaTag}
-      <ProductPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-      />
+      <ProductPageTemplate image={frontmatter.image} title={frontmatter.title} />
     </Layout>
-  );
-};
+  )
+}
 
 ProductPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object
-    })
-  })
-};
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
 
-export default ProductPage;
+export default ProductPage
 
 export const productPageQuery = graphql`
   query ProductPage($id: String!) {
@@ -137,4 +136,4 @@ export const productPageQuery = graphql`
       }
     }
   }
-`;
+`
