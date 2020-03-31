ea7fd38de8fd11c2f038fe41e471f58e2ce944b3
Matthew Solomonson <ggmatt@gmail.com>
2020-03-31 09:48:53 -0400

    Replace table on partners page with Airtable view

--- src/components/InstitutionsList.jsx
+++ src/components/InstitutionsList.jsx
@@ -1,28 +1,47 @@
-import React from "react";
-const data = require("../data.json");
+import React, { useState, useEffect } from 'react'
+// const data = require('../data.json')
 
 const InstitutionsList = () => {
-  const rows = data.map(
-    ({ study_biobank, coordinator, city_country }, index) => (
-      <tr key={`item-${index}`}>
-        <td>{study_biobank}</td>
-        <td>{coordinator}</td>
-        <td>{city_country}</td>
-      </tr>
+  const [airTable, setAirTable] = useState(null)
+  useEffect(() => {
+    setAirTable(
+      <iframe
+        class="airtable-embed"
+        src="https://airtable.com/embed/shrlpTPNjEw7AQjtt?backgroundColor=blue&viewControls=on"
+        frameborder="0"
+        onmousewheel=""
+        width="100%"
+        height="2500px"
+        style={{
+          background: 'transparent',
+          border: '1px solid #ccc',
+        }}
+      ></iframe>
     )
-  );
-  return (
-    <table className="table is-striped">
-      <thead>
-        <tr>
-          <th>Study</th>
-          <th>Coordinator</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-};
+  }, [])
 
-export default InstitutionsList;
+  return airTable
+
+  // const rows = data.map(({ study_biobank, coordinator, city_country }, index) => (
+  //   <tr key={`item-${index}`}>
+  //     <td>{study_biobank}</td>
+  //     <td>{coordinator}</td>
+  //     <td>{city_country}</td>
+  //   </tr>
+  // ))
+
+  // return (
+  //   <table className="table is-striped">
+  //     <thead>
+  //       <tr>
+  //         <th>Study</th>
+  //         <th>Coordinator</th>
+  //         <th>Location</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>{rows}</tbody>
+  //   </table>
+  // )
+}
+
+export default InstitutionsList
