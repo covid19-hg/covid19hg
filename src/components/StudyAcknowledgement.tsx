bd7bd45b109363b08574588584e456095f137fcf
Huy Nguyen <me@huy.dev>
2020-07-01 13:41:29 -0400

    Allow multiple adhoc groups
    This is to add the "in silico" group in addition to the "coordination" team.

---
+++ src/components/StudyAcknowledgement.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
+import { Link as GatsbyLink } from "gatsby";
+import { getPartnerPath } from "./partnersPageUtils";
+import { DisplayedStudy, RoleList } from "./acknowledgementUtils";
+import { sortByLastName } from "./acknowledgementUtils";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  studyRoot: {
+    marginTop: theme.spacing(2),
+  },
+  roleTitle: {
+    marginTop: theme.spacing(1),
+  },
+}));
+
+interface LargeScreenColumnProps {
+  roleLists: RoleList[];
+}
+
+export const LargeScreenColumn = ({ roleLists }: LargeScreenColumnProps) => {
+  const classes = useStyles();
+  const elems = roleLists.map(({ name: roleName, contributors }) => {
+    const sorted = sortByLastName(contributors).map((name, index) => (
+      <Typography key={index}>{name}</Typography>
+    ));
+    return (
+      <React.Fragment key={roleName}>
+        <Typography variant="h6" component="h3" className={classes.roleTitle}>
+          {roleName}
+        </Typography>
+        {sorted}
+      </React.Fragment>
+    );
+  });
+  return (
+    <Grid item={true} xs={6}>
+      {elems}
+    </Grid>
+  );
+};
+
+interface Props {
+  study: DisplayedStudy;
+  linkToPartnersPage: boolean;
+  isLargeScreen: boolean;
+}
+
+const StudyAcknowledgement = ({
+  study,
+  linkToPartnersPage,
+  isLargeScreen,
+}: Props) => {
+  const { id: studyId, name: studyName, leftRoleLists, rightRoleLists } = study;
+  const classes = useStyles();
+  let title: React.ReactElement<any>;
+  if (linkToPartnersPage === true) {
+    title = (
+      <Typography
+        variant="h5"
+        component={GatsbyLink}
+        to={getPartnerPath(studyId)}
+        gutterBottom={true}
+      >
+        {studyName}
+      </Typography>
+    );
+  } else {
+    title = (
+      <Typography variant="h5" component="h3" gutterBottom={true}>
+        {studyName}
+      </Typography>
+    );
+  }
+
+  let content: React.ReactNode;
+  if (isLargeScreen) {
+    const leftColumn = <LargeScreenColumn roleLists={leftRoleLists} />;
+    const rightColumn = <LargeScreenColumn roleLists={rightRoleLists} />;
+    content = (
+      <Grid container={true} spacing={2}>
+        {leftColumn}
+        {rightColumn}
+      </Grid>
+    );
+  } else {
+    content = leftRoleLists.map(({ name: roleName, contributors }) => {
+      const sorted = sortByLastName(contributors).map((name, index) => (
+        <Typography key={index}>{name}</Typography>
+      ));
+      return (
+        <React.Fragment key={roleName}>
+          <Typography variant="h6" component="h3" gutterBottom={true}>
+            {roleName}
+          </Typography>
+          {sorted}
+        </React.Fragment>
+      );
+    });
+  }
+  return (
+    <Card className={classes.studyRoot}>
+      <CardContent>
+        {title}
+        {content}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default StudyAcknowledgement;
--- src/components/acknowledgementUtils.ts
+++ src/components/acknowledgementUtils.ts
@@ -2,17 +2,59 @@ import _sortBy from "lodash/sortBy";
 import _sumBy from "lodash/sumBy";
 import _minBy from "lodash/minBy";
 import _last from "lodash/last";
-import { RoleList } from "./AcknowlegementPageContent";
+import { RawContributor, RawStudy } from "./AcknowlegementPageContent";
+import _partition from "lodash/partition";
+import _uniq from "lodash/uniq";
+
+interface BaseContributor {
+  name: string;
+  role: string;
+  affiliation: string;
+  affiliationLink: string | undefined;
+  id: string;
+}
+
+enum ContributorType {
+  Study,
+  Adhoc,
+}
+interface StudyContributor extends BaseContributor {
+  type: ContributorType.Study;
+  studyIds: string[];
+}
+interface AdhocContributor extends BaseContributor {
+  type: ContributorType.Adhoc;
+  adhocGroup: string;
+}
+
+export interface ProcessedStudy {
+  name: string;
+  contributorsByRole: Map<string, string[]>;
+}
+
+export type ProcessedStudies = Map<string, ProcessedStudy>;
+
+export interface RoleList {
+  name: string;
+  contributors: string[];
+}
+export interface DisplayedStudy {
+  name: string;
+  id: string;
+  approxRoleListsHeight: number;
+  leftRoleLists: RoleList[];
+  rightRoleLists: RoleList[];
+}
+
+export type Contributor = StudyContributor | AdhocContributor;
 
 export const sortByLastName = (names: string[]) =>
   _sortBy(names, (name) => _last(name.toLowerCase().split(" ")));
 
-export const adminTeamName = "COVID-19 Host Genetics Initiative Coordination";
-
 // Approximate height of a role's title compared to the height of a line of
 // normal paragaph text:
 const approxHeightOfRoleTitle = 2;
-export const getRoleListApproxHeight = ({ contributors }: RoleList) =>
+const getRoleListApproxHeight = ({ contributors }: RoleList) =>
   contributors.length + approxHeightOfRoleTitle;
 
 enum Side {
@@ -92,4 +134,194 @@ export const getPartitionPointToMinimizeDifferences = <T>(
     ({ difference }) => difference
   )!.partitionIndex;
   return [items.slice(0, min + 1), items.slice(min + 1)];
+};
+
+const approxHeightOfStudyTitle = 4;
+const getStudyApproxHeight = ({ approxRoleListsHeight }: DisplayedStudy) =>
+  approxRoleListsHeight + approxHeightOfStudyTitle;
+
+const getDisplayedStudy = (
+  { name: studyName, contributorsByRole }: ProcessedStudy,
+  id: string,
+  isLargeScreen: boolean
+): DisplayedStudy => {
+  const unprocessedRoleList: RoleList[] = _sortBy(
+    [...contributorsByRole.entries()].map(([role, contributors]) => ({
+      name: role,
+      contributors: _uniq(contributors),
+    })),
+    ({ name }) => name
+  );
+  let leftRoleLists: RoleList[];
+  let rightRoleLists: RoleList[];
+  if (isLargeScreen) {
+    const [left, right] = splitIntoTwoBalancedLists(
+      unprocessedRoleList,
+      getRoleListApproxHeight
+    );
+    leftRoleLists = left;
+    rightRoleLists = right;
+  } else {
+    leftRoleLists = unprocessedRoleList;
+    rightRoleLists = [];
+  }
+  const approxRoleListsHeight = Math.max(
+    _sumBy(leftRoleLists, getRoleListApproxHeight),
+    _sumBy(rightRoleLists, getRoleListApproxHeight)
+  );
+  return {
+    approxRoleListsHeight,
+    id,
+    leftRoleLists,
+    rightRoleLists,
+    name: studyName,
+  };
+};
+
+export const processContributorList = (
+  contributors: RawContributor[],
+  studies: RawStudy[],
+  isLargeScreen: boolean
+) => {
+  const unsortedProcessedStudies: ProcessedStudies = new Map();
+  const studyLookup = new Map(
+    studies.map(({ id, name }) => [id, name] as const)
+  );
+  const studyContributors: StudyContributor[] = [];
+  const adhocContributors: AdhocContributor[] = [];
+  for (const item of contributors) {
+    if ("studyIds" in item) {
+      const {
+        contributor,
+        role,
+        affiliation,
+        affiliationLink,
+        id,
+        studyIds,
+      } = item;
+      studyContributors.push({
+        name: contributor,
+        role,
+        affiliation,
+        affiliationLink,
+        id,
+        studyIds,
+        type: ContributorType.Study,
+      });
+    } else if ("adhocGroup" in item) {
+      const {
+        contributor,
+        role,
+        affiliation,
+        affiliationLink,
+        id,
+        adhocGroup,
+      } = item;
+      adhocContributors.push({
+        name: contributor,
+        role,
+        affiliation,
+        affiliationLink,
+        id,
+        adhocGroup,
+        type: ContributorType.Adhoc,
+      });
+    }
+  }
+
+  for (const { studyIds, role, name: contributorName } of studyContributors) {
+    for (const studyId of studyIds) {
+      const studyName = studyLookup.get(studyId);
+      if (studyName !== undefined) {
+        const retrievedStudy = unsortedProcessedStudies.get(studyId);
+        let study: ProcessedStudy;
+        if (retrievedStudy === undefined) {
+          study = {
+            name: studyName,
+            contributorsByRole: new Map(),
+          };
+          unsortedProcessedStudies.set(studyId, study);
+        } else {
+          study = retrievedStudy;
+        }
+
+        const retrievedContributorsInThisRole = study.contributorsByRole.get(
+          role
+        );
+        let contributorsInThisRole: string[];
+        if (retrievedContributorsInThisRole === undefined) {
+          contributorsInThisRole = [];
+          study.contributorsByRole.set(role, contributorsInThisRole);
+        } else {
+          contributorsInThisRole = retrievedContributorsInThisRole;
+        }
+        contributorsInThisRole.push(contributorName);
+      }
+    }
+  }
+  const processedStudies = _sortBy(
+    [...unsortedProcessedStudies.entries()],
+    ([, { name }]) => name.toLowerCase()
+  );
+
+  const processedAdhocGroups: ProcessedStudies = new Map();
+  for (const { adhocGroup, role, name: contributorName } of adhocContributors) {
+    const retrievedStudy = processedAdhocGroups.get(adhocGroup);
+    let study: ProcessedStudy;
+    if (retrievedStudy === undefined) {
+      study = {
+        name: adhocGroup,
+        contributorsByRole: new Map(),
+      };
+      processedAdhocGroups.set(adhocGroup, study);
+    } else {
+      study = retrievedStudy;
+    }
+
+    const retrievedContributorsInThisRole = study.contributorsByRole.get(role);
+    let contributorsInThisRole: string[];
+    if (retrievedContributorsInThisRole === undefined) {
+      contributorsInThisRole = [];
+      study.contributorsByRole.set(role, contributorsInThisRole);
+    } else {
+      contributorsInThisRole = retrievedContributorsInThisRole;
+    }
+    contributorsInThisRole.push(contributorName);
+  }
+
+  const displayedProcessedStudies = processedStudies.map(([id, study]) =>
+    getDisplayedStudy(study, id, isLargeScreen)
+  );
+
+  let leftColumnStudies: DisplayedStudy[], rightColumnStudies: DisplayedStudy[];
+  if (isLargeScreen) {
+    const [left, right] = getPartitionPointToMinimizeDifferences(
+      displayedProcessedStudies,
+      getStudyApproxHeight
+    );
+    const joined = [...left, ...right];
+    leftColumnStudies = [];
+    rightColumnStudies = [];
+    for (let i = 0; i < joined.length; i += 1) {
+      const study = joined[i];
+      if (i % 2 == 0) {
+        leftColumnStudies.push(study);
+      } else {
+        rightColumnStudies.push(study);
+      }
+    }
+  } else {
+    leftColumnStudies = displayedProcessedStudies;
+    rightColumnStudies = [];
+  }
+
+  const displayedAdhocGroups = [...processedAdhocGroups.values()].map((group) =>
+    getDisplayedStudy(group, "", isLargeScreen)
+  );
+
+  return {
+    leftColumnStudies,
+    rightColumnStudies,
+    adhocGroups: displayedAdhocGroups,
+  };
 };
