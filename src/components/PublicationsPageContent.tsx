b5872085c4bf0c1f3e9609d83c7166461db69c9c
Huy Nguyen <huy-nguyen@users.noreply.github.com>
2020-11-18 10:43:26 -0500

    Add publications page content (#87)

---
+++ src/components/PublicationsPageContent.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+import { Typography, Card, CardContent, Theme, Link } from "@material-ui/core";
+import { Container, Grid } from "./materialUIContainers";
+import { makeStyles } from "@material-ui/styles";
+import { Link as GatsbyLink } from "gatsby";
+
+interface Publication {
+  name: string;
+  posted_date: string;
+  links: { description: string; url: string }[];
+  cases_description: string | null;
+  controls_description: string | null;
+  genome_wide_significant_loci: string | null;
+  first_publication: string | null;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  leftColumn: {
+    textAlign: "right",
+    fontWeight: "bold",
+  },
+  grid: {
+    alignItems: "center",
+  },
+  card: {
+    margin: theme.spacing(2, 0),
+  },
+  link: {
+    textDecoration: "underline",
+    marginRight: theme.spacing(1),
+  },
+}));
+
+interface Props {
+  title: string;
+  intro: string;
+  publications: Publication[];
+}
+
+const PublicationsPageContent = ({ intro, publications }: Props) => {
+  const classes = useStyles();
+  // TODO: need to allow multiple links for each study (preprint vs published paper)
+  const publicationElems = publications.map(
+    ({
+      name,
+      posted_date,
+      cases_description,
+      controls_description,
+      genome_wide_significant_loci,
+      first_publication,
+      links,
+    }) => {
+      const leftColumnSize = 2;
+      const rightColumnSize = 10;
+      const linkElems = links.map(({ description, url }) => {
+        const urlElem = url.startsWith("/") ? (
+          <GatsbyLink key={url} className={classes.link} to={url}>
+            {description}
+          </GatsbyLink>
+        ) : (
+          <Link key={url} href={url} className={classes.link}>
+            {description}
+          </Link>
+        );
+        return urlElem;
+      });
+      const casesDescriptionElem =
+        cases_description === null ? null : (
+          <>
+            <Grid
+              item={true}
+              xs={leftColumnSize}
+              className={classes.leftColumn}
+            >
+              Cases:
+            </Grid>
+            <Grid item={true} xs={rightColumnSize}>
+              {cases_description}
+            </Grid>
+          </>
+        );
+      const controlsDescriptionElem =
+        controls_description === null ? null : (
+          <>
+            <Grid
+              item={true}
+              xs={leftColumnSize}
+              className={classes.leftColumn}
+            >
+              Controls:
+            </Grid>
+            <Grid item={true} xs={rightColumnSize}>
+              {controls_description}
+            </Grid>
+          </>
+        );
+      const lociElem =
+        genome_wide_significant_loci === null ? null : (
+          <>
+            <Grid
+              item={true}
+              xs={leftColumnSize}
+              className={classes.leftColumn}
+            >
+              Genome-wide significant loci:
+            </Grid>
+            <Grid item={true} xs={rightColumnSize}>
+              {genome_wide_significant_loci}
+            </Grid>
+          </>
+        );
+      const firstPublicationElem =
+        first_publication === null ? null : (
+          <>
+            <Grid
+              item={true}
+              xs={leftColumnSize}
+              className={classes.leftColumn}
+            >
+              First publication:
+            </Grid>
+            <Grid item={true} xs={rightColumnSize}>
+              {first_publication}
+            </Grid>
+          </>
+        );
+      return (
+        <Card key={name} className={classes.card}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom={true}>
+              {name}
+            </Typography>
+            <Grid container={true} spacing={2} className={classes.grid}>
+              <Grid
+                item={true}
+                xs={leftColumnSize}
+                className={classes.leftColumn}
+              >
+                Posted date:
+              </Grid>
+              <Grid item={true} xs={rightColumnSize}>
+                {posted_date}
+              </Grid>
+              <Grid
+                item={true}
+                xs={leftColumnSize}
+                className={classes.leftColumn}
+              >
+                Links:
+              </Grid>
+              <Grid item={true} xs={rightColumnSize}>
+                {linkElems}
+              </Grid>
+              {casesDescriptionElem}
+              {controlsDescriptionElem}
+              {lociElem}
+              {firstPublicationElem}
+            </Grid>
+          </CardContent>
+        </Card>
+      );
+    }
+  );
+  return (
+    <Container marginTop={1} fixed={true}>
+      <Typography>{intro}</Typography>
+      {publicationElems}
+    </Container>
+  );
+};
+
+export default PublicationsPageContent;
---
+++ src/pages/publications/index.md
@@ -0,0 +1,72 @@
+---
+templateKey: publications-page
+title: COVID-19 Genetics Publications
+publications_page_intro: Many of the participating groups in COVID-19 Host
+  Genetics Initiative have already submitted or published genetic findings
+  during or in advance of.  These findings should be cited as relevant to
+  follow-up work based on specific discovered loci, even if that work may have
+  been based on unpublished analyses of the HGI.
+studies:
+  - posted_date: 20th October 2020
+    name: COVID-19 HGI data freeze 4
+    links:
+      - description: Website
+        url: /results/
+  - posted_date: 25th September 2020
+    name: Genetics Of Mortality In Critical Care (GenOMICC consortium)
+    links:
+      - description: Preprint
+        url: https://www.medrxiv.org/content/10.1101/2020.09.24.20200048v2
+    cases_description: 2244 critically-ill Covid-19 patients from 208 UK intensive
+      care units (ICUs)
+    controls_description: population controls from UKBB, 100K Genomes, Generation Scotland
+    genome_wide_significant_loci: " SLC6A20/CXCR6 locus, OAS1/2/3 locus, DPP9 locus,
+      TYK2 locus, IFNAR2/IL10RB/IFNAR1 locus, HLA-G locus, CCHCR1 locus, NOTCH4
+      locus"
+    first_publication: OAS1/2/3 locus, DPP9 locus, TYK2 locus, IFNAR2/IL10RB/IFNAR1
+      locus, HLA-G locus, CCHCR1 locus, NOTCH4 locus
+  - posted_date: 9th October 2020
+    name: Roberts, Park et al
+    links:
+      - description: Preprint
+        url: https://www.medrxiv.org/content/10.1101/2020.10.06.20205864v1
+    cases_description: 2407 self-reported positive nasopharyngeal swab test, 250 of
+      those reporting hospitalization
+    controls_description: 14,933 COVID-19 negative and 1,967 COVID-19 Cases
+      Reporting NO Hospitalization
+    genome_wide_significant_loci: SLC6A20/CXCR6 locus, ABO locus, IVNS1ABP locus
+      (male-specific), SRRM1 locus, Immunoglobulin Lambda Locus locus
+    first_publication: IVNS1ABP locus (male-specific), SRRM1 locus, Immunoglobulin
+      Lambda Locus locus
+  - posted_date: 7th September 2020
+    name: Shelton, Shastri et al
+    links:
+      - description: Preprint
+        url: https://www.medrxiv.org/content/10.1101/2020.09.04.20188318v1
+    cases_description: 15,434 self-reported positive test result, and of those,
+      1,131 reported hospitalization
+    controls_description: 101,268
+    genome_wide_significant_loci: SLC6A20/CXCR6 locus, ABO locus
+  - posted_date: 2nd July 2020
+    name: COVID-19 HGI data freeze 3
+    links:
+      - description: Website
+        url: /results/
+  - posted_date: 2nd June 2020
+    name: Ellinghaus, Degenhardt et al.
+    links:
+      - description: Preprint
+        url: https://www.medrxiv.org/content/10.1101/2020.05.31.20114991v1
+      - description: Paper
+        url: https://www.nejm.org/doi/full/10.1056/NEJMoa2020283
+    cases_description: 1,980 patients with Covid-19 respiratory failure from seven
+      centers in Italy and Spain
+    controls_description: "population controls from Italy and Spain (n=2205) "
+    genome_wide_significant_loci: SLC6A20/CXCR6 locus, ABO locus
+    first_publication: SLC6A20/CXCR6 locus, ABO locus
+  - posted_date: 15th May 2020
+    name: COVID-19 HGI data freeze 2
+    links:
+      - description: Website
+        url: /results/
+---
