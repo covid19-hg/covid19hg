9967f0e76a92e62f50aca57c1f6331a88c30944b
Huy Nguyen <me@huy.dev>
2021-02-24 10:58:55 -0500

    Fix eslint errors

--- fetchContributors/fetchAdditionalContributorsData.js
+++ fetchContributors/fetchAdditionalContributorsData.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 const Airtable = require("airtable");
 
 Airtable.configure({
--- fetchContributors/index.js
+++ fetchContributors/index.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 const fetchAdditionalContributorsData = require("./fetchAdditionalContributorsData");
 const fs = require("fs");
 const path = require("path");
